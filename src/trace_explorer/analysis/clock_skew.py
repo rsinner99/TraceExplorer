25767fd1232b74fa4abba38ad375c31d17fa2a68
rsinner99 <robin.sinner@t-online.de>
2022-01-30 16:55:31 +0100

    Clock adjustment: Child span execution should be in the middle of parent spans.

--- src/trace_explorer/analysis/clock_skew.py
+++ src/trace_explorer/analysis/clock_skew.py
@@ -5,13 +5,15 @@ from trace_explorer.definitions import logs
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def get_time_shift(parent_time, child_time):
+def get_time_shift(parent_time, parent_duration, child_time, child_duration):
     """
-    Calculates the time difference and adds 1.
+    Calculates the time difference and necessary shift.
     Returns the necessary time shift in microseconds.
+    Child execution should be in the middle of the parent span.
     """
-    diff = parent_time - child_time
-    return diff + 1
+    start_diff = parent_time - child_time
+    duration_diff = int((parent_duration - child_duration) / 2)
+    return start_diff + duration_diff
 
 def adjust_timestamps(span, shift):
     """
@@ -31,8 +33,13 @@ def adjust_clock_skew(parent, child):
     if parent.start_time < child.start_time:
         return
 
-    shift = get_time_shift(parent.start_time, child.start_time)
-    adjust_timestamps(child)
+    shift = get_time_shift(
+        parent.start_time, 
+        parent.duration, 
+        child.start_time, 
+        child.duration
+    )
+    adjust_timestamps(child, shift)
     logger.info(
         'Timestamp adjusted in Trace %s: Service "%s" with Span ID "%s"', 
         child.trace_id, child.service, child.span_id
